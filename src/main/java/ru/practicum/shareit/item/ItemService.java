2dac84147fbc17cdeb597bda8a8544769814c80c
Ilya Vetrenko <litiychet@yandex.ru>
2024-02-19 00:11:52 +1000

    fix: check style fix

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface ItemService {
     ItemDto create(Long userId, ItemDto itemDto);
+
     ItemDto update(Long userId, Long itemId, ItemDto itemDto);
+
     ItemDto get(Long itemId);
+
     List<ItemDto> getUserItems(Long userId);
+
     List<ItemDto> searchItem(String text);
 }
